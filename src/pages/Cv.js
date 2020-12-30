1e755b224663a58df4e4ace80fdacae6a2fb3d1c
dor-co <dor713cohen@gmail.com>
2020-12-31 01:56:15 +0200

    tenn

--- src/pages/Cv.js
+++ src/pages/Cv.js
@@ -10,8 +10,8 @@ function Cv() { //after click on 'Home' in the menu this page will show
           <button className='cvDownBtn'>Download my CV <IoIcons.IoMdDownload/></button>
           </a>
       </div>
-      <div>
-        <img src="./Pap.png" alt="cv img" class='imgCv' width="300px"/>
+      <div className='imgCv'>
+        <img src="../Pap.png" width="300px"/>
       </div>
     </div>
   )
