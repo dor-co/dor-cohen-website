843f07c5e12a466a8c70e63dffa851feec84c59b
dor-co <dor713cohen@gmail.com>
2021-01-03 01:33:47 +0200

    commit

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -16,7 +16,7 @@ function Projects() { //after click on 'Products' in the menu this page will sho
       </div>
 
         <div class="container1">
-          <a href="https://github.com/dor-co/Logo-Quiz-Game">
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/dor-co/Logo-Quiz-Game">
             <img src="./Bmw.png" alt="bmw img" class="image"/>
             <div class="overlay">
               <h3 class="text">Logo Quiz Game</h3>
@@ -25,7 +25,7 @@ function Projects() { //after click on 'Products' in the menu this page will sho
         </div>
 
         <div class="container1">
-          <a href="https://github.com/dor-co/Burger-Order-App"> 
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/dor-co/Burger-Order-App"> 
             <img src="./Bur.jpg" alt="burger img" class="image"/>
             <div class="overlay">
               <h3 class="text">Burger Order App</h3>
@@ -34,7 +34,7 @@ function Projects() { //after click on 'Products' in the menu this page will sho
         </div>
 
         <div class="container2">
-          <a href="https://github.com/dor-co/Delivery-Sport-Products"> 
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/dor-co/Delivery-Sport-Products"> 
             <img src="./Tot.png" alt="football img" class="image"/>
             <div class="overlay">
               <h3 class="text">Delivery Sports Products</h3>
